643fb7638c141c631811f4f3c66c54ea97be987f
jihad0a <jihadffa0@gmail.com>
2026-07-27 15:36:35 +0600

    clean: remove unused deploy.js

--- deploy.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-const result = execSync('npx vercel --token $VERCEL_TOKEN --yes --prod 2>&1', {
-  cwd: 'C:/project/folio-tailwind-1.0.0',
-  encoding: 'utf8',
-  timeout: 60000
-});
-console.log(result);
